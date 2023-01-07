7de7e76965b43321d17d64f18f3c65dce6bde7da
Bruna Leonel <psbru2012@gmail.com>
2023-01-06 21:53:11 -0300

    criando adicionar, editar, cancelar

--- js/scripts.js
+++ js/scripts.js
@@ -4,12 +4,75 @@ const todoInput = document.querySelector("#input");
 const todoList = document.querySelector("#todo-list");
 const editForm = document.querySelector("#edit-form");
 const editInput = document.querySelector("#edit-input");
-const tcalcelEdit = document.querySelector("#cancel-edit");
+const calcelEdit = document.querySelector("#cancel-edit");
+const calcelEditBtn = document.querySelector("#cancel-edit-btn");
+const pesquisarPorTitulo = document.querySelector("pesquisar")
+
+let oldInputValue;
 
 //Funções:
 const saveTodo = (texto) => {
-    const todo = document.createElement("div")
-    todo.classList.add("todo")
+    const todo = document.createElement("div");
+    todo.classList.add("todo");
+
+    const todoTitle = document.createElement("h3");
+    todoTitle.innerText = texto;
+    todo.appendChild(todoTitle);
+
+    console.log(todoList);
+
+    const doneBtn = document.createElement("button");
+    doneBtn.classList.add("finish-todo");
+    doneBtn.innerHTML = '<i class="fa-solid fa-check"></i>';
+    todo.appendChild(doneBtn);
+
+    const editBtn = document.createElement("button");
+    editBtn.classList.add("edit-todo");
+    editBtn.innerHTML = '<i class="fa-solid fa-pen"></i>';
+    todo.appendChild(editBtn);
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.classList.add("remove-todo");
+    deleteBtn.innerHTML = '<i class="fa-solid fa-xmark"></i>';
+    todo.appendChild(deleteBtn)
+
+    todoList.appendChild(todo);
+
+    //Limpar o valor após digitar:
+    todoInput.value = "";
+    todoInput.focus();
+};
+
+const toggleForms = () => {
+    todoForm.classList.toggle("hide");
+    todoList.classList.toggle("hide");
+};
+
+const updateTodo = (text) => {
+    const todos = document.querySelectorAll(".todo");
+
+    todos.forEach((todo => {
+        let todoTitle = todo.querySelector("h3")
+
+        if (todoTitle.innerText === oldInputValue) {
+            todoTitle.innerText = text;
+        }
+    }))
+}
+
+const search = (text) => {
+    const todos = document.querySelectorAll(".todo");
+
+    todos.forEach((todo => {
+        const todoTitle = todo.querySelector("h3").innerText.toLowerCase();
+
+        console.log(todoTitle);
+
+    }));
+};
+
+const shouwAllItems = () => {
+    
 }
 
 //Eventos:
@@ -21,3 +84,60 @@ todoForm.addEventListener("submit", (e) => {
         saveTodo(inputValue)
     }
 });
+
+document.addEventListener("click", (e) => {
+    const targetEl = e.target;
+    //Selecionar a div mais próxima ao comando
+    const parentEl = targetEl.closest("div");
+    //variável pra receber o titulo da edição
+    let todoTitle;
+
+    if (parentEl && parentEl.querySelector("h3")) {
+        todoTitle = parentEl.querySelector("h3").innerText;
+    }
+
+    if(targetEl.classList.contains("finish-todo")) {
+        parentEl.classList.toggle("done")     //Ao clicar várias vezes irá continuar add classes "done" por isso usa-se toggle no lugar de add
+    }
+
+    if(targetEl.classList.contains("remove-todo")) {
+        parentEl.remove();
+    }
+
+    if(targetEl.classList.contains("edit-todo")){
+        toggleForms();
+
+        editInput.value = todoTitle;
+        oldInputValue = todoTitle;
+    }
+});
+
+calcelEditBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    toggleForms();
+});
+
+editForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const editInputValue = editInput.value;
+
+    if(editInputValue) {
+        //atualizar
+        updateTodo(editInputValue);
+    }
+
+    toggleForms();
+});
+
+pesquisarPorTitulo.addEventListener("input", (text) => {
+    const str = this.value;
+    console.log(str);
+
+    if(str) {
+        filterData(str);
+    } else {
+        shouwAllItems();
+    }
+})
